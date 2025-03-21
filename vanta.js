7f91e71bebf9f3b49edb9a1de6c1918f7d0506fc
Tanisha <tanisha.bamne1980@gmail.com>
2025-03-21 23:12:45 +0530

    update

--- vanta.js
+++ vanta.js
@@ -10,3 +10,14 @@ VANTA.NET({
     color: 0xcf0808,
     backgroundColor: 0x7fafaf
   })
+
+  VANTA.NET({
+    el: "#footer",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.00,
+    minWidth: 200.00,
+    scale: 1.00,
+    scaleMobile: 1.00
+  })
